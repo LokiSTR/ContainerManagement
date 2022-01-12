1aaa96ff7ebcdf01b6c6b69df2ec3493891eecb7
AmiTh004 <90444032+AmiTh004@users.noreply.github.com>
2022-01-12 13:15:25 +0100

    controller

--- ContainerManagement/src/controller/GueterTransporter.java
+++ ContainerManagement/src/controller/GueterTransporter.java
@@ -1,5 +1,34 @@
 package controller;
 
-public class GueterTransporter {
+import java.util.ArrayList;
+
+import model.Gueter.Frischware;
+import model.Gueter.Gueter;
+
+public class GueterTransporter extends ClassController{
+
+    ArrayList<Gueter> gueterAr;
+
+    public GueterTransporter(MainController mc) {
+        super(mc);
+        setGueterAr(new ArrayList<Gueter>());
+    }
+
+    public void createDemodata() {
+        getGueterAr().add(new Frischware(235, 23, "24.01.2022"));
+        
+    }
+
+    /**
+     * 
+     * SETTER UND GETTER
+     */
+
+     public void setGueterAr(ArrayList<Gueter> gueterAr) {
+         this.gueterAr = gueterAr;
+     }
+     public ArrayList<Gueter> getGueterAr() {
+         return gueterAr;
+     }
     
 }
--- ContainerManagement/src/controller/MainController.java
+++ ContainerManagement/src/controller/MainController.java
@@ -1,5 +1,20 @@
 package controller;
 
+import view.Output;
+
 public class MainController {
     
+    Output output;
+
+    /**
+     * 
+     * SETTER UND GETTER
+     */
+
+     public void setOutput(Output output) {
+         this.output = output;
+     }
+     public Output getOutput() {
+         return output;
+     }
 }
