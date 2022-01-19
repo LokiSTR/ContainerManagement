a41ecf27f3cb006e2762899630ccea17a1aacd40
LokiSTR <bent.fenske@gmail.com>
2022-01-19 10:36:13 +0100

    MainScene

--- ContainerManagement/src/view/MainWindow.java
+++ ContainerManagement/src/view/MainWindow.java
@@ -1,3 +1,5 @@
+package view;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -6,33 +8,28 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
-public class App extends Application {
+public class MainWindow extends Application {
     public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
- 
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+    StackPane root = new StackPane();
+    root.getChildren().add(btn);
   
   /*
   Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
   Scene scene = new Scene(root);
   */
   
-  Scene scene = new Scene(root, 300, 250);
+    Scene scene = new Scene(root, 300, 250);
   
-  primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
- 
- public static void main(String[] args) {
-        launch(args);
+    primaryStage.setTitle("Container Management");
+    primaryStage.setScene(scene);
+    primaryStage.show();
     }
 }
