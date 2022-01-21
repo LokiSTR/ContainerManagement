8bf762305c9e0179ac463f49b289885c6231e8f4
LokiSTR <bent.fenske@gmail.com>
2022-01-21 10:08:47 +0100

    21.1.22

--- ContainerManagement/src/App.java
+++ ContainerManagement/src/App.java
@@ -1,17 +1,23 @@
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
 public class App extends Application {
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+        
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
  
+            @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
@@ -25,12 +31,14 @@ public class App extends Application {
   Scene scene = new Scene(root);
   */
   
-  Scene scene = new Scene(root, 300, 250);
+    Scene scene = new Scene(root, 300, 250);
   
-  primaryStage.setTitle("Hello World!");
+    primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+    
+
  
  public static void main(String[] args) {
         launch(args);
--- ContainerManagement/src/controller/MainSceneController.java
+++ ContainerManagement/src/controller/MainSceneController.java
@@ -1,13 +1,13 @@
 package controller;
-
+//Für JavaFX benöigt
 import javafx.event.ActionEvent;
 
 public class MainSceneController {
-
+    //Erster Button 
     void newGut(ActionEvent event) {
 
     }
-
+    //Zweiter Button
     void newVerschiffen(ActionEvent event) {
 
     }
